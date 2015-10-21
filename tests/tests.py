dcd6ba6f3e9453d7c52e1375b3f59a17e2bc87f5
bziegler <benediktziegler@gmail.com>
2015-10-21 13:50:05 +0200

    updated tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -2,4 +2,43 @@ from __future__ import unicode_literals, print_function, division, absolute_impo
 
 import unittest
 
+from xrdtools import read_xrdml
+
+
+class TestXrdmlRead(unittest.TestCase):
+
+    def test_read_xrdml_area_scan(self):
+        data = read_xrdml('test_area.xrdml')
+
+        self.assertEqual(data['comment'], {'1': None})
+        self.assertEqual(data['kAlpha1'], 1.540598)
+        self.assertEqual(data['kAlpha2'], 1.544426)
+        self.assertEqual(data['kType'], 'K-Alpha 1')
+        self.assertEqual(data['sample'], 'B11091')
+        self.assertEqual(data['slitHeight'], 0.38)
+        self.assertEqual(data['stepAxis'], 'Omega')
+        self.assertEqual(data['X'], [ 2.37])
+        self.assertEqual(data['Phi'], [0.])
+        self.assertEqual(data['Psi'], [-0.4])
+        self.assertEqual(data['yunit'], 'nd')
+        self.assertEqual(data['xunit'], 'nd')
+        self.assertEqual(data['xlabel'], 'Omega-2Theta')
+        self.assertEqual(data['status'], 'Completed')
+        self.assertEqual(data['ylabel'], 'Omega')
+        self.assertEqual(data['hkl'], {'h': 0, 'k': 1, 'l': 3})
+        self.assertEqual(data['Y'], [-0.98])
+        self.assertEqual(data['Z'], [9.695])
+        self.assertEqual(data['measType'], 'Area measurement')
+        self.assertEqual(data['Lambda'], 1.540598)
+        self.assertEqual(data['scanAxis'], 'Omega-2Theta')
+        self.assertEqual(data['substrate'], 'SrTiO3')
+        self.assertEqual(data['kBeta'], 1.39225)
+        self.assertEqual(data['time'], [3.])
+        self.assertEqual(data['kAlphaRatio'], 0.5)
+        self.assertEqual(data['2Theta'].size, 5700)
+        self.assertEqual(data['Omega'].size, 5700)
+        self.assertEqual(data['data'].size, 5700)
+
+
+
 
