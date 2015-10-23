f326c29dd7a1e4ebe70140bdbf326b426125cec2
bziegler <benediktziegler@gmail.com>
2015-10-23 14:27:53 +0200

    added test

--- tests/tests.py
+++ tests/tests.py
@@ -8,14 +8,78 @@ from xrdtools.io import validate_xrdml_schema
 
 
 class TestXrdmlRead(unittest.TestCase):
-
     def test_schema(self):
         filename = os.path.abspath('tests/test_area.xrdml')
 
         version = validate_xrdml_schema(filename)
         self.assertEqual(version, 1.0)
 
-    def test_read_xrdml_area_scan(self):
+    def test_read_xrdml_scan(self):
+        filename = os.path.abspath('tests/test_scan.xrdml')
+
+        data = read_xrdml(filename)
+
+        self.assertEqual(data['comment'], {u'1': u''})
+        self.assertEqual(data['kAlpha1'], 1.540598)
+        self.assertEqual(data['kAlpha2'], 1.544426)
+        self.assertEqual(data['kBeta'], 1.39225)
+        self.assertEqual(data['kAlphaRatio'], 0.0)
+        self.assertEqual(data['kType'], 'K-Alpha 1')
+        self.assertEqual(data['Lambda'], 1.540598)
+
+        self.assertEqual(data['slitHeight'], 0.19)
+        self.assertEqual(data['scanAxis'], '2Theta-Omega')
+        self.assertEqual(data['time'].size, 750)
+
+        self.assertEqual(data['sample'], 'B10135')
+
+        self.assertEqual(data['x'].size, 750)
+        self.assertEqual(data['2Theta'].size, 750)
+        self.assertEqual(data['Omega'].size, 750)
+        self.assertEqual(data['Phi'], 0.0)
+        self.assertEqual(data['Psi'], -0.6)
+        self.assertEqual(data['Y'], 0.26)
+        self.assertEqual(data['X'], 1.17)
+        self.assertEqual(data['Z'], 9.607)
+        self.assertEqual(data['data'].size, 750)
+
+        self.assertEqual(data['xunit'], 'deg')
+        self.assertEqual(data['xlabel'], '2Theta-Omega')
+        self.assertEqual(data['status'], 'Completed')
+        self.assertEqual(data['scannb'], [0])
+        self.assertEqual(data['measType'], 'Scan')
+
+        keys = ['comment',
+                'kAlpha1',
+                'kAlpha2',
+                'kType',
+                'sample',
+                'slitHeight',
+                'Phi',
+                'Psi',
+                'filename',
+                'xunit',
+                'xlabel',
+                'Omega',
+                'status',
+                '2Theta',
+                'scannb',
+                'Y',
+                'X',
+                'Z',
+                'data',
+                'measType',
+                'Lambda',
+                'scanAxis',
+                'kBeta',
+                'time',
+                'x',
+                'kAlphaRatio']
+
+        for key in keys:
+            self.assertIn(key, data.keys())
+
+    def test_read_xrdml_area(self):
         filename = os.path.abspath('tests/test_area.xrdml')
 
         data = read_xrdml(filename)
